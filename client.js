93ea6ab074a018fce9056e5711ec028c8de210db
Griego86 <dimi1086@hotmail.com>
2023-10-18 16:08:57 -0700

    added username via conn.write

--- client.js
+++ client.js
@@ -4,7 +4,12 @@ const net = require("net");
 const connect = () => {
   const conn = net.createConnection({
     host: 'localhost',
-    port: 50541
+    port: 50541,
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server"),
+    conn.write("Name: DAA")
   });
 
   conn.on("data", (data) => console.log(data));
